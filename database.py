fdd7afa0aa2651e61a90861570abf2030dd508f7
pseudorex <asmitgupta309@gmail.com>
2025-09-25 02:50:41 +0530

    Backend with changed database file

--- database.py
+++ database.py
@@ -1,11 +1,31 @@
+# from sqlalchemy import create_engine
+# from sqlalchemy.orm import sessionmaker
+# from sqlalchemy.ext.declarative import declarative_base
+#
+# SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:244901@localhost/newNormalized'
+#
+# engine = create_engine(SQLALCHEMY_DATABASE_URL)
+#
+# SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+#
+# Base = declarative_base()
+
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:244901@localhost/newNormalized'
+# Get database URL from environment variable
+SQLALCHEMY_DATABASE_URL = os.environ.get("DATABASE_URL")
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+# Create SQLAlchemy engine with SSL (required for many remote Postgres)
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL,
+    connect_args={"sslmode": "require"}  # add this if your DB requires SSL
+)
 
+# Create session factory
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
+# Base class for models
 Base = declarative_base()
